c398f4a4ee7a6f7df5b072b5be3d01d7b98e9924
Timofey Tychkov <timofeytychkov@gmail.com>
2023-09-29 15:13:36 +0700

    Добавьте файлы проекта.

---
+++ Лаба 2.2.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define MAX_RECORDS 100
+#define MAX_EMPLOYEE 20
+#define MAX_ORDER 10
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <conio.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+
+}
